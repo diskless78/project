c5e68a3ebee6c19b1a9c7423ea30cdb0561dd343
Marcus Duong <141284683+0975830459@users.noreply.github.com>
2025-10-24 14:35:28 +0700

    add mongo

---
+++ cxview/mongo/command.js
@@ -0,0 +1,20 @@
+db.createUser(
+   {
+     user: "minh.pham",
+     pwd: passwordPrompt(),
+     roles: [
+        { role: "dbOwner", db: "cxview" }
+     ]
+   }
+)
+
+
+db.createUser(
+   {
+     user: "len.vo",
+     pwd: passwordPrompt(),
+     roles: [
+        { role: "dbOwner", db: "cxview" }
+     ]
+   }
+)
